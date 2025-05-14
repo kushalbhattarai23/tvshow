9ba3e47e75279ced4ddd2a55ad479940847c87f7
kushalbhattarai23 <45483709+kushalbhattarai23@users.noreply.github.com>
2025-05-14 07:04:15 +0545

    Add TV show routes and update components

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import Home from './components/Home/Home';
 import SignIn from './components/Auth/SignIn';
 import SignUp from './components/Auth/SignUp';
 import Dashboard from './components/Dashboard/Dashboard';
-import ShowDetails from './components/ShowDetails/ShowDetails';
 import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
@@ -21,14 +20,6 @@ function App() {
             element={
               <ProtectedRoute>
                 <Dashboard />
-              </ProtectedRoute>
-            } 
-          />
-          <Route 
-            path="/:showName" 
-            element={
-              <ProtectedRoute>
-                <ShowDetails />
               </ProtectedRoute>
             } 
           />
