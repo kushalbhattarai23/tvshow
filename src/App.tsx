169d53fc83804fa22e6d276dc61ea48a01199a18
kushalbhattarai23 <45483709+kushalbhattarai23@users.noreply.github.com>
2025-05-16 22:12:28 +0545

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
           <Route 
-            path="/dashboard" 
+            path="/" 
             element={
               <ProtectedRoute>
                 <Dashboard />
